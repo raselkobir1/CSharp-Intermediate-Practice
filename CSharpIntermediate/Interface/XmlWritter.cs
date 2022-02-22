a75ed07091dc7aa37c3564bb2417aeec13dd47bb
raselkobir1 <raselkobir29@gmail.com>
2022-02-23 01:24:56 +0600

    working on interface

---
+++ CSharpIntermediate/Interface/XmlWritter.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpIntermediate.Interface
+{
+
+    public class XmlWritter : FileBase, IWriter
+    {
+        public void WriteFile()
+        {
+            Console.WriteLine("Writing file in the XmlWriter class.");
+        }
+        public override void SetName()
+        {
+            base.SetName();
+            Console.WriteLine("Setting name in the XmlWriter class.");
+        }
+    }
+    public class JsonWriter : FileBase, IWriter
+    {
+        public void WriteFile()
+        {
+            Console.WriteLine("Writing file in the JSON-Writer class.");
+        }
+        public override void SetName()
+        {
+            base.SetName();
+            Console.WriteLine("Setting name in the JsonWriter class.");
+        }
+    }
+
+
+    public class FileBase
+    {
+        public virtual void SetName()
+        {
+            Console.WriteLine("Settings name isn the base wiriter class.");
+        }
+    }
+}
+
