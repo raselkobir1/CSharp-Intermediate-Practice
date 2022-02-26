d2222db4e4b0780e568b0d7b68a14baa23d9fb13
raselkobir1 <raselkobir29@gmail.com>
2022-02-26 18:25:04 +0600

    working on dictionary

---
+++ CSharpIntermediate/DictionaryCollection/DictionaryExample.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpIntermediate.DictionaryCollection
+{
+    public class DictionaryExample
+    {
+        public void GetDictionaryValue()
+        {
+            Dictionary<string,int> dictionary_name = new Dictionary<string,int>(); 
+        }
+    }
+}
+
+
+/*
+ In C#, Dictionary is a generic collection which is generally used to store key/value pairs. 
+The working of Dictionary is quite similar to the non-generic hashtable. 
+The advantage of Dictionary is, it is generic type. Dictionary is defined under 
+System.Collection.Generic namespace. It is dynamic in nature means the size of the dictionary is 
+grows according to the need.
+
+Important Points:
+-----------------
+The Dictionary class implements the
+IDictionary<TKey,TValue> Interface
+IReadOnlyCollection<KeyValuePair<TKey,TValue>> Interface
+IReadOnlyDictionary<TKey,TValue> Interface
+IDictionary Interface
+In Dictionary, the key cannot be null, but value can be.
+In Dictionary, key must be unique. Duplicate keys are not allowed if you try to use duplicate key then compiler will throw an exception.
+In Dictionary, you can only store same types of elements.
+The capacity of a Dictionary is the number of elements that Dictionary can hold.
+How to create the Dictionary?
+Dictionary class has 7 constructors which are used to create the Dictionary, here we only use Dictionary<TKey, TValue>() constructor and if you want to learn more about constructors then refer C# | Dictionary Class.
+
+Dictionary<TKey, TValue>(): This constructor is used to create an instance of the Dictionary<TKey, TValue> class that is empty, has the default initial capacity, and uses the default equality comparer for the key type as follows:
+ */
--- CSharpIntermediate/HashTableCollection/HashTableExaample.cs
+++ CSharpIntermediate/HashTableCollection/HashTableExaample.cs
@@ -38,3 +38,23 @@ namespace CSharpIntermediate.HashTableCollection
         }
     }
 }
+
+/* Hashtable
+A Hashtable is a collection of key/value pairs that are arranged based on the hash code of the key. 
+Or in other words, a Hashtable is used to create a collection which uses a hash table for storage. 
+It generally optimized the lookup by calculating the hash code of every key and store into another
+basket automatically and when you accessing the value from the hashtable at that time it matches 
+the hashcode with the specified key. It is the non-generic type of collection which is defined in 
+System.Collections namespace. 
+
+Important Points: 
+------------------
+1.In Hashtable, the key cannot be null, but value can be.
+2.In Hashtable, key objects must be immutable as long as they are used as keys in the Hashtable.
+3.The capacity of a Hashtable is the number of elements that Hashtable can hold.
+4.A hash function is provided by each key object in the Hashtable.
+5.The Hashtable class implements the IDictionary, ICollection, IEnumerable, ISerializable, IDeserializationCallback, and ICloneable interfaces.
+6.In hashtable, you can store elements of the same type and of the different types.
+7.The elements of hashtable that is a key/value pair are stored in DictionaryEntry, so you can also cast the key/value pairs to a DictionaryEntry.
+8.In Hashtable, key must be unique. Duplicate keys are not allowed.
+ */
